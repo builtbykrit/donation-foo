4db2729e388dc0bf39e774fd2e25c1953a66d0df
Garrett Vangilder <garrett.vangilder@gmail.com>
2019-07-15 13:15:27 -0400

    improves walk performance by excluding already travelled paths

--- utils/walk.js
+++ utils/walk.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 
+const executedOn = [];
 const walk = function(dir, done) {
     let results = [];
     fs.readdir(dir, function(err, list) {
@@ -7,16 +9,21 @@ const walk = function(dir, done) {
         let i = 0;
         (function next() {
             let file = list[i++];
-            if (!file) return done(null, results);
+            if (!file) {
+                return done(null, results);
+            }
+            let fileName = path.basename(file);
             file = dir + '/' + file;
             fs.stat(file, function(err, stat) {
-                if (stat && stat.isDirectory()) {
+                if (stat && stat.isDirectory() && !executedOn.includes(fileName)) {
                     walk(file, function(err, res) {
                         results = results.concat(res);
+                        executedOn.push(fileName);
                         next();
                     });
                 } else {
                     results.push(file);
+                    executedOn.push(fileName);
                     next();
                 }
             });
